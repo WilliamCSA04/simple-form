3b3aa2d875e2ff93981fe7744b7ea65b4d750131
Williamcsa04 <williamcaetano14@gmail.com>
2020-07-17 22:45:11 -0300

    :sparkles: Added globalStyles as decorator

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import Button from '.';
+import { GlobalStyles } from '../../styles';
 
 export default {
   title: 'Button',
-  parameters: {
-    info: { inline: true },
-  },
+  decorators: [
+    (storyFn) => (
+      <>
+        <GlobalStyles />
+        {storyFn()}
+      </>
+    ),
+  ],
 };
 
 export const Default = () => <Button onClick={(e) => e.preventDefault}>Press me</Button>;
