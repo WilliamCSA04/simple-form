984ca3e1ea6262e04377ec07a565b789ced4ac02
Williamcsa04 <williamcaetano14@gmail.com>
2020-07-27 21:02:44 -0300

    :lipstick: styled basic form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export interface Props {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
@@ -7,10 +8,23 @@ export interface Props {
   id?: string;
 }
 
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  border-radius: 3px;
+  background-color: #3d3b8e;
+  padding: 1em;
+  margin: auto;
+  & > * {
+    margin: 1em;
+  }
+`;
+
 export default function Form({ children, onSubmit, className, id }: Props) {
   return (
-    <form id={id} className={className} onSubmit={onSubmit}>
+    <StyledForm id={id} className={className} onSubmit={onSubmit}>
       {children}
-    </form>
+    </StyledForm>
   );
 }
